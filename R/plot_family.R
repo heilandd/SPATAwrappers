74608185a2d0bd6b36fc770a61fa376a879bef38
heilandd <dieter.henrik.heiland@uniklinik-freiburg.de>
2022-02-03 11:31:30 +0100

    nn

--- R/plot_family.R
+++ R/plot_family.R
@@ -1366,7 +1366,15 @@ plot2DInterpolation <- function(object,
     
     if(addImage==T){p=SPATA2::plotSurface(object, display_image=T, pt_alpha = 0)}else{p=ggplot()+theme_void()}
     scCoords$pred <- round(scCoords$pred, digits = 0) 
-    scCoords$pred <- factor(scCoords$pred, levels=coords_df %>% pull(!!sym(color_by)) %>% unique())
+    
+    
+    levels <- data.frame(pred=unique(z), real=coords_df[,color_by] %>% unique() %>% pull(!!sym(color_by)))
+    levels <- levels %>% filter(pred %in% unique(scCoords$pred))
+    scCoords$pred <- as.factor(scCoords$pred)
+    scCoords$pred <- factor(scCoords$pred,  labels = levels$real)
+    
+
+    
     p=p+geom_point(data=scCoords, 
                    aes(x=x, y=y, color=pred), 
                    size=pt.size, 
